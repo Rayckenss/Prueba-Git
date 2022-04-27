ab425f8512276ba25d66fa378fefd3946c3fa61f
Rayckens <andres.galeano8@misena.edu.co>
2022-04-26 20:40:50 -0500

    Actividad realizada

--- Actividad1.cs
+++ Actividad1.cs
@@ -10,6 +10,12 @@ public class Actividad1 : MonoBehaviour
     [Header("Actividad 2")]
     public float cateto1, cateto2;
     private float hipotenusa;
+    [Header("Actividad 3")]
+    public int tiempo;
+    private int horas, minutos;
+    [Header ("Actividad 4")]
+    public float calificacion1, calificacion2, calificacion3, examenFinal, trabajoFinal;
+    private float promedioCalificaciones, notaEstudiante;
 
     // Start is called before the first frame update
     void Start()
@@ -22,9 +28,14 @@ public class Actividad1 : MonoBehaviour
         //Actividad 2
         hipotenusa = Mathf.Sqrt((cateto1 * cateto1) + (cateto2 * cateto2));
         Debug.Log("la hipotenusa es "+hipotenusa);
-        //actividad 3 aaa
-
-
+        //Actividad 3
+        horas = tiempo / 60;
+        minutos = tiempo % 60;
+        Debug.Log(tiempo + " minutos son " + horas + " horas y " + minutos + " minutos");
+        //Actividad 4
+        promedioCalificaciones = (calificacion1 + calificacion2 + calificacion3) / 3;
+        notaEstudiante = (promedioCalificaciones * 0.55f) + (examenFinal * 0.3f) + (trabajoFinal * 0.15f);
+        Debug.Log("La nota del estudiante fue " + notaEstudiante);
     }
 
     // Update is called once per frame
